9052d962af3fde8d34e6b1d22314ddecabf6eca4
VsSekorin <vssekorin@gmail.com>
2020-05-29 21:53:47 +0300

    Refactoring

--- src/fsmtore/core.clj
+++ src/fsmtore/core.clj
@@ -10,7 +10,7 @@
 
 (defn command? [line] (not (or (str/blank? line) (str/starts-with? line "#"))))
 
-(defn get-data [filename]
+(defn data [filename]
   (with-open [rdr (io/reader filename)]
     (let [[[s] fins & lines] (map #(str/split % #"\s+") (filter command? (line-seq rdr)))]
      [(union #{[start e s]} (set (map #(vector % e fin) fins)) (set lines))
@@ -26,15 +26,16 @@
     [p q]))
 
 (defn +edges [commands [pair & tail]]
-  (let [[[pv pa s] [_ qa qv]] pair [_ a _ :as lp] (edge commands s s) [_ o _ :as pq] (edge commands pv qv)
-        w (str (if pq "(") (cond (= pa e) qa (= qa e) pa :else (str pa (if lp (str a "*")) qa)) (if pq (str "+" o ")")))]
-    (if pair (+edges (-> commands (disj pq) (conj [pv w qv])) tail) commands)))
+  (if (nil? pair) commands
+    (let [[[pv pa s] [_ qa qv]] pair [_ a _ :as lp] (edge commands s s) [_ o _ :as pq] (edge commands pv qv)
+          w (str (if pq "(") (cond (= pa e) qa (= qa e) pa :else (str pa (if lp (str a "*")) qa)) (if pq (str "+" o ")")))]
+      (+edges (-> commands (disj pq) (conj [pv w qv])) tail))))
 
 (defn convert [[commands [state & tail]]]
-  (let [prs (pairs state commands) ucs (apply disj (+edges commands prs) (apply concat prs))]
-    (if state (convert [ucs tail]) commands)))
+  (if (nil? state) commands
+    (let [prs (pairs state commands)] (convert [(apply disj (+edges commands prs) (apply concat prs)) tail]))))
 
 (defn print-res [commands] (let [[_ res _] (edge commands start fin)] (println res)))
 
-(defn -main [filename & args] (-> (get-data filename) (convert) (print-res)))
+(defn -main [filename & args] (print-res (convert (data filename))))
 
